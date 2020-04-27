be77bd451dab8bcccb5adc9fbda07df677ab3f1f
royal1er <florianallebe6@gmail.com>
2020-04-27 01:03:58 -0400

    Modification accueil

--- accueil.php
+++ accueil.php
@@ -31,30 +31,6 @@ include("header.php");
   ?>
   <!-- Fin slides -->
   <!-- On affiche la liste des pays par continent si un continent est sélectionner -->
-  <div class="container">
-  <?php
-  if(isset($_GET['Continent']))
-  {
-    $selectedContinent = $_GET['Continent'];
-    ?>
-    <h1 class=" text-center mb-3">Les pays en <?php echo $selectedContinent ?> </h1>
-    <?php
-            $desPays = getCountriesByContinent($selectedContinent);
-         ?>
-       <code>
-         <!-- <?php var_dump($desPays[0]); ?> -->
-         <?php
-         echo '<div class="row">';
-          foreach ($desPays as $desPays) { ?>
-
-            <?php echo '<div class="col-xl-4 col-lg-4 col-md-4 col-sm-6 col-12 mb-3 ">
-                              <a class="text-light " href="infos.php?Name='.$desPays->Name.'"><button type="button" class="btn btn-info w-100 h-100 shadow p-3 mb-5 rounded-lg">'.$desPays->Name.'</button></a>
-                        </div>';
-     }
-     echo '</div>';
-    }    ?>
-      </code>
-  </div>
 <?php
 require_once 'javascripts.php';
 require_once 'footer.php';
